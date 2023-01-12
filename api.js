8caf37dc62ce8d501735c4134379f135c4f766f6
gabrielduartea <gaaguilar384@gmail.com>
2023-01-11 22:56:01 -0300

    tentativa

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "https://sistema-estagio-back-production.up.railway.app/api/v1",
+  baseURL: "http://sistema-estagio-back-production.up.railway.app/api/v1",
   headers: [
     {
       "source": "/api/(.*)",
