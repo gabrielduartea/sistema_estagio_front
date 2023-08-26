fdafa171bfef287addda55ade4ec0f37b8d36347
gabrielduartea <gaaguilar384@gmail.com>
2023-08-26 04:34:40 -0300

    implementação relatorios e melhorias

--- .nuxt/dist/server/server.js
+++ .nuxt/dist/server/server.js
@@ -7808,7 +7808,7 @@ async function setContext(app, context) {
   // If context not defined, create it
   if (!app.context) {
     app.context = {
-      isStatic: true,
+      isStatic: false,
       isDev: false,
       isHMR: false,
       app,
@@ -10116,9 +10116,6 @@ const layouts = {
     },
     isFetching() {
       return this.nbFetching > 0;
-    },
-    isPreview() {
-      return Boolean(this.$options.previewData);
     }
   },
   methods: {
