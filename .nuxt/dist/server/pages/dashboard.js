f840c0d24a78ef202a8d4014fd7d9d4c524a6f5d
gabrielduartea <gaaguilar384@gmail.com>
2023-10-02 21:51:34 -0300

    generate

---
+++ .nuxt/dist/server/pages/dashboard.js
@@ -0,0 +1,55 @@
+exports.ids = [8];
+exports.modules = {
+
+/***/ 259:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vuetify-loader/lib/loader.js??ref--4!./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--7!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/dashboard.vue?vue&type=template&id=a81173da&
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', [_vm._ssrNode("<h1>DASHBOARD</h1>")]);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./pages/dashboard.vue?vue&type=template&id=a81173da&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/dashboard.vue?vue&type=script&lang=js&
+/* harmony default export */ var dashboardvue_type_script_lang_js_ = ({});
+// CONCATENATED MODULE: ./pages/dashboard.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_dashboardvue_type_script_lang_js_ = (dashboardvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(13);
+
+// CONCATENATED MODULE: ./pages/dashboard.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_dashboardvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "1e9f662e"
+  
+)
+
+/* harmony default export */ var dashboard = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=dashboard.js.map
